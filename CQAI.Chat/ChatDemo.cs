fd47954b4d729776b8089102303b84e51c241668
Administrator <Administrator@CQ>
2024-05-23 11:25:46 +0800

    添加项目文件。

---
+++ CQAI.Chat/ChatDemo.cs
@@ -0,0 +1,78 @@
+﻿using MoonshotClient;
+using MoonshotDotnet;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQAI.Chat
+{
+    public static class ChatDemo
+    {
+        public static async Task<MessagesItem?> Chat(this List<MessagesItem> history, MoonshotClientDemo moonshotClient, string? modelId, string? input)
+        {
+            var userInput = new MessagesItem
+            {
+                role = "user",
+                content = input
+            };
+            history.Add(userInput);
+
+            var chatRep = new ChatReq
+            {
+                max_tokens = 2048,
+                temperature = 0.5,
+                top_p = 1,
+                frequency_penalty = 0,
+                presence_penalty = 0,
+                model = modelId,
+                messages = history
+            };
+
+            //Console.WriteLine($"{chatRep.messages.Find(s => s.role == "user")?.content}");
+
+            var chatResp = await moonshotClient.Chat(chatRep);
+            var chatRespBody = await chatResp.Content.ReadAsStringAsync();
+            var chatRespModel = JsonConvert.DeserializeObject<MoonshotRes>(chatRespBody);
+            var message = chatRespModel?.choices[0].message;
+            history.Add(message);
+            return message;
+        }
+    
+
+        public static async Task Output(this string msg)
+        {
+            foreach (char c in msg)
+            {
+                Console.Write(c); // 逐字输出
+                Thread.Sleep(50); 
+            }
+            Console.WriteLine("");
+        }
+
+        /// <summary>
+        /// 初始化模型角色
+        /// </summary>
+        /// <param name="history"></param>
+        /// <param name="content"></param>
+        /// <returns></returns>
+        public static async Task Init(this List<MessagesItem> history, string content = null)
+        {
+            if (content == null)
+            {
+                content = "你是 Kimi，由 Moonshot AI 提供的人工智能助手，你更擅长中文对话。你会为用户提供安全，有帮助，准确的回答。";
+            }
+
+            history.Add(new MessagesItem
+            {
+                role = "system",
+                content = content
+            });
+        }
+
+    
+    }
+}
---
+++ CQAI.Chat/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using CQAI.Chat;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using MoonshotDotnet;
+
+//以下两个配置必须填写正确
+
+//公开的服务地址,月之暗面：https://api.moonshot.cn
+MoonshotClientDemo.Host = "https://api.moonshot.cn";
+//您在平台上创建的 API Key
+MoonshotClientDemo.ApiKey = "sk-";
+
+
+var history = new List<MessagesItem>();
+var services = new ServiceCollection();
+services.AddHttpClient();
+var serviceProvider = services.BuildServiceProvider();
+var logger = serviceProvider.GetService<ILogger<MoonshotClientDemo>>();
+var httpClientFactory = serviceProvider.GetService<IHttpClientFactory>();
+var moonshotClient = new MoonshotClientDemo(logger, httpClientFactory);
+
+// ListModels 
+var modelsResp = await moonshotClient.ListModels();
+var modelId = modelsResp.data[0]?.id;
+Console.WriteLine($"模型：{modelId} 创建成功\r\n");
+
+await history.Init();
+Console.WriteLine($"我是 Kimi～很高兴遇见你！有什么可以帮助你的吗？\r\n");
+while (true)
+{
+    Console.WriteLine("");
+    var input = Console.ReadLine();
+    Console.WriteLine("");
+
+    MessagesItem? message = await history.Chat(moonshotClient, modelId, input);
+    message?.content.Output();
+
+}
